b6b21f7fd0ec9f157751200b0573543708a5f10a
Matthijs <mzondervan@hotmail.nl>
2023-11-29 12:58:34 +0100

    clean up a bit and add some styling

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,23 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {HelloComponentComponent} from "./hello-component/hello-component.component";
 import {NavBarComponent} from "./nav-bar/nav-bar.component";
-import {Product} from "./models/product";
 import {ProductDetailComponent} from "./product-detail/product-detail.component";
+import {ProductCardComponent} from "./product-card/product-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HelloComponentComponent, NavBarComponent, ProductDetailComponent],
+  imports: [CommonModule, RouterOutlet, NavBarComponent, ProductDetailComponent, ProductCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'IPRWC';
-
-  Producten: Product[] = [
-    new Product("Product 1", 1.00),
-    new Product("Product 2", 2.00),
-  ]
 }
--- front-end/src/app/product-detail/product-detail.component.ts
+++ front-end/src/app/product-detail/product-detail.component.ts
@@ -1,17 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {Product} from "../models/product";
+import {NavBarComponent} from "../nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavBarComponent],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
-export class ProductDetailComponent  implements OnInit{
-  product: Product;
-
-  constructor() {}
+export class ProductDetailComponent{
 
 }
