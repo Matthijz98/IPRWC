df16c0309b89850d22b76a143d0c94345c57949d
Matthijs <mzondervan@hotmail.nl>
2023-11-30 16:08:22 +0100

    start working on the backend and the database

---
+++ backend/src/main/java/com/iprwc/backend/controllers/ProductController.java
@@ -0,0 +1,5 @@
+package com.iprwc.backend.controllers;
+
+public class ProductController {
+
+}
---
+++ backend/src/main/java/com/iprwc/backend/entities/Product.java
@@ -0,0 +1,14 @@
+package com.iprwc.backend.entities;
+
+//import java.util.Objects;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.Id;
+
+//@Entity
+public class Product {
+//
+//    private @Id @GeneratedValue Long id;
+
+}
