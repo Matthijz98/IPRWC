53e9a5bd714cd764fb84b24f635628d609cd316c
Matthijs Zondervan <mzondervan@hotmail.nl>
2024-01-12 12:10:18 +0100

    add to card button on details page

--- front-end/src/app/webshop/product/product-detail/product-detail.component.ts
+++ front-end/src/app/webshop/product/product-detail/product-detail.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import {NavBarComponent} from "../../nav-bar/nav-bar.component";
 import {ProductService} from "../../../services/product.service";
 import {Product} from "../../../interfaces/product";
+import {CartService} from "../../../services/card.service";
 
 @Component({
   selector: 'app-product-detail',
@@ -17,19 +18,34 @@ export class ProductDetailComponent implements OnInit {
   product: any
   productId: number = 0
 
-  constructor(private productsService: ProductService, private route: ActivatedRoute) {
+  buttonText: string = 'Add to cart';
+  buttonClass: string = 'mt-4 bg-gray-900 text-white px-4 py-2 rounded';
+
+  constructor(private productsService: ProductService, private route: ActivatedRoute, private cartService: CartService) {
   }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.productId = params['id'];
-      console.log(this.productId);
       this.productsService.get_product(this.productId).subscribe((product: Product) => {
         this.product = product;
       });
     });
+  }
 
+  addToCart(product: Product) {
+    this.cartService.addProduct(product);
 
+    this.buttonText = 'Added to cart';
+    this.buttonClass = 'mt-4 bg-green-500 text-white px-4 py-2 rounded';
+
+    setTimeout(() => {
+      this.buttonText = 'Add to cart';
+    }, 2000);
+
+    setTimeout(() => {
+      this.buttonClass = 'mt-4 bg-gray-900 text-white px-4 py-2 rounded';
+    }, 2000);
   }
 
 }
